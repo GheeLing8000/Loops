5dd16402c355b8726ae43afba5471694eb2922ba
GheeLing8000 <37270192+GheeLing8000@users.noreply.github.com>
2025-11-02 12:04:28 +0800

    Add files via upload

---
+++ CircleComputation.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class CircleComputation {
+
+    public static void main(String[] args) {
+          // Declare variables
+      double radius, diameter, circumference, area;  // inputs and results - all in double
+        Scanner in = new Scanner(System.in);  // Scan the keyboard
+      // Prompt and read inputs as "double"
+      System.out.print("Enter the radius: ");
+      radius = in.nextDouble();  // read input as double
+        in.close();
+
+      // Compute in "double"
+      diameter = 2.0 * radius;
+      circumference = 2.0 * Math.PI * radius;
+      area = Math.PI * radius * radius;
+
+
+      // Print results using printf() with the following format specifiers:
+      //   %.2f for a double with 2 decimal digits
+      //   %n for a newline
+      System.out.printf("Diameter is: %.2f%n", diameter);
+      System.out.printf("Circumference is: %.2f%n", circumference);
+      System.out.printf("Area is: %.2f%n", area);
+    }
+}
