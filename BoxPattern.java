d5f38e4426f0a3ad761f58c08cb5a472e22f2712
GheeLing8000 <37270192+GheeLing8000@users.noreply.github.com>
2025-11-05 20:52:05 +0800

    Add files via upload

---
+++ BoxPattern.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class BoxPattern {
+    public static void main(String[] args) {
+        //Declare the variables
+        final int size; //size of pattern to be input
+        //Prompt for size and read input as "int"
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the size: ");
+        size = in.nextInt();
+        in.close();
+         /*Pattern (a)
+        // Outer loop to print each of the rows
+        for (int row = 1; row <= size; row++) {  // row = 1, 2, 3, ..., size
+            // Inner loop to print each of the columns of a particular row
+            for (int col = 1; col <= size; col++) {  // col = 1, 2, 3, ..., size
+                if (row == 1 || row == size || col == 1 || col == size) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");  // Need to print the "leading" blanks
+                }
+            }
+            // Print a newline after printing all the columns
+            System.out.println();
+        } */
+        /*Pattern (b)
+        // Outer loop to print each of the rows
+        for (int row = 1; row <= size; row++) {  // row = 1, 2, 3, ..., size
+            // Inner loop to print each of the columns of a particular row
+            for (int col = 1; col <= size; col++) {  // col = 1, 2, 3, ..., size
+                if (row == 1 || row == size || row == col) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");  // Need to print the "leading" blanks
+                }
+            }
+            // Print a newline after printing all the columns
+            System.out.println();
+        }*/
+        /*Pattern c
+        // Outer loop to print each of the rows
+        for (int row = 1; row <= size; row++) {  // row = 1, 2, 3, ..., size
+            // Inner loop to print each of the columns of a particular row
+            for (int col = 1; col <= size; col++) {  // col = 1, 2, 3, ..., size
+                if (row == 1 || row == size || (row + col) == size + 1) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");  // Need to print the "leading" blanks
+                }
+            }
+            // Print a newline after printing all the columns
+            System.out.println();
+        }*/
+        /*Pattern d
+        // Outer loop to print each of the rows
+        for (int row = 1; row <= size; row++) {  // row = 1, 2, 3, ..., size
+            // Inner loop to print each of the columns of a particular row
+            for (int col = 1; col <= size; col++) {  // col = 1, 2, 3, ..., size
+                if (row == 1 || row == size || (row + col) == size + 1 || row == col) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");  // Need to print the "leading" blanks
+                }
+            }
+            // Print a newline after printing all the columns
+            System.out.println();
+        }*/
+        // Outer loop to print each of the rows
+        for (int row = 1; row <= size; row++) {  // row = 1, 2, 3, ..., size
+            // Inner loop to print each of the columns of a particular row
+            for (int col = 1; col <= size; col++) {  // col = 1, 2, 3, ..., size
+                if (row == 1 || row == size || (row + col) == size + 1 || row == col || col == 1 || col == size) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");  // Need to print the "leading" blanks
+                }
+            }
+            // Print a newline after printing all the columns
+            System.out.println();
+        }
+    }
+}
