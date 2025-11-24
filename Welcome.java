52e4d6063c2b54962de0a7bd0c0b6b80c52f8f47
GheeLing8000 <37270192+GheeLing8000@users.noreply.github.com>
2025-11-24 21:26:10 +0800

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+public class Welcome {
+	public static void main(String[] args){
+		System.out.println("W             W  W             W  W             W");
+		System.out.println(" W     W     W    W     W     W    W     W     W ");
+		System.out.println("  W   W W   W      W   W W   W      W   W W   W  ");
+		System.out.println("   W W   W W        W W   W W        W W   W W   ");
+		System.out.println("    W     W          W     W          W     W    ");                                                              
+	}
+}
