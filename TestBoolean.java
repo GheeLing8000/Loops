1e3f4122eb529eb9f2e8711841c64d25fb27333a
GheeLing8000 <37270192+GheeLing8000@users.noreply.github.com>
2025-12-15 20:52:04 +0800

    Add files via upload

---
+++ TestBoolean.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TestBoolean {
+    public static void main(String[] args) {
+       //Create a scanner
+        Scanner input = new Scanner(System.in);
+        //Receive an input
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        System.out.print("Is " + number + "\n\tdivisible by 2 and 3? " +
+                (number % 2 == 0 && number % 3 == 0)
+                + "\n\tdivisible by 2 or 3? " +
+                (number % 2 == 0 || number % 3 == 0) +
+                "\n\tdivisible by 2 or 3, but not both? "
+                + (number % 2 == 0 ^ number % 3 == 0));
+
+        }
+
+}
