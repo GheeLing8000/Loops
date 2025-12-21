28810d876394cb22177e542ecad4a68f35a8cbc8
GheeLing8000 <37270192+GheeLing8000@users.noreply.github.com>
2025-12-21 16:48:22 +0800

    Add files via upload

--- ComputeSumAndAverageOfPositiveNegativeNumbers.java
+++ ComputeSumAndAverageOfPositiveNegativeNumbers.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ComputeSumAndAverageOfPositiveNegativeNumbers {
     public static void main(String[] args) {
-       //Create a scanner
+        //Create a scanner
         Scanner input = new Scanner(System.in);
         //Read an initial data
         System.out.print(
@@ -11,7 +11,9 @@ public class ComputeSumAndAverageOfPositiveNegativeNumbers {
         //Keep reading the data until the input is 0
         int sum = 0;
         int counter = 0;
-        while (data != 0){
+
+        double average = 0;
+        while (data != 0) {
             //find the total after including int data sum = sum + data
             sum += data;
             //Increase the count
@@ -20,10 +22,12 @@ public class ComputeSumAndAverageOfPositiveNegativeNumbers {
             System.out.print(
                     "Enter an int value (the program exits if the input is 0): ");
             data = input.nextInt();
+            average = (double) sum / counter;
         }
+
         System.out.println("The sum is " + sum);
         System.out.println("The number of count is " + counter);
-
+        System.out.println("The average is " + average);
 
 
     }
