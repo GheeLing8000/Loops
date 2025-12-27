31effaf1db7f6e6037edb0e39415a31924cf3e3c
GheeLing8000 <37270192+GheeLing8000@users.noreply.github.com>
2025-12-27 12:51:45 +0800

    Add files via upload

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -9,11 +9,20 @@ public class GreatestCommonDivisor {
         System.out.print("Enter the second integer: ");
         int n2 = input.nextInt();
         int gcd = 1;
-        int k = 2;
+        /*int k = 2;
         while ( k <= n1 && k <= n2){
             if (n1 % k == 0 && n2 % k == 0)
                 gcd = k;
             k++;
+        }*/
+        /*for (int k = 2; k <= n1 && k  <= n2; k++){
+            if (n1 % k == 0 && n2 % k == 0)
+                gcd = k;
+
+        }*/
+        for (int k = 2; k <= n1/2 && k <= n2/2; k++){
+            if (n1 % k == 0 && n2 % k == 0)
+                gcd = k;
         }
         System.out.println("The greatest common divisor for " +n1 +
                 " and " + n2 + " is " + gcd);
